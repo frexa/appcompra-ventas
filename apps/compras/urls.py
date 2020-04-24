37a890981066633bf3b38e74579e3ff8f708e62b
frexa <poppingale18@gmail.com>
2020-04-24 11:03:20 -0430

    modulo de actualizacion de compras

--- apps/compras/urls.py
+++ apps/compras/urls.py
@@ -6,4 +6,8 @@ urlpatterns=[
 		path('compra',RegistrarCompra.as_view(), name='registrar-compra'),
 		path('ingrediente',RegistrarIngrediente.as_view(),name='registrar-ingrediente'),
 		path('linea',RegistrarLineaCompra.as_view(),name='registrar-linea')])),
+	path('actualizar/',include([
+		path('<int:pk>/compra',ActualizarCompra.as_view(),name = 'actualizar-compra'),
+		path('<int:pk>/linea',ActualizarLineaCompra.as_view(),name = 'actualizar-linea'),
+		path('<int:pk>/ingrediente',ActualizarIngrediente.as_view(),name = 'actualizar-ingrediente')])),
 	]
--- apps/compras/views.py
+++ apps/compras/views.py
@@ -32,8 +32,54 @@ class RegistrarCompra(CreateView):
 		else:
 			return render_to_response(self.get_context_data(form=form))
 
+class ActualizarCompra(UpdateView):
+	"""docstring for ActualizarCompra"""
+	model = Compra
+	template_name = 'CrearCompra.html'
+	form_class = CompraForm
+	success_url = 'ingrediente'
+
+	def get_context_data(self, **kwargs):
+		context = super(RegistrarCompra,self).get_context_data(**kwargs)
+		if 'form' not in context:
+			context['form'] = self.model_form(self.request.GET)
+		return context
+
+	def post(self, request, *args, **kwargs):
+		self.object = self.get_object
+		form = self.form_class(self.request.POST)
+		if form.is_valid():
+			compra = form.save(commit=False)
+			compra.save()
+			return HttpResponseRedirect(self.get_success_url())
+		else:
+			return render_to_response(self.get_context_data(form=form))
+
 class RegistrarLineaCompra(CreateView):
 	"""docstring for RegistrarCompra"""
+	model = LineaDeCompra
+	template_name = 'CrearLineaDeCompra.html'
+	form_class = LineaDeCompraForm
+	success_url = 'compra'
+
+	def get_context_data(self, **kwargs):
+		context = super(RegistrarLineaCompra,self).get_context_data(**kwargs)
+		if 'form' not in context:
+			context['form'] = self.form_class(self.request.GET)
+		return context
+
+	def post(self, request, *args, **kwargs):
+		self.object = self.get_object
+		form = self.form_class(self.request.POST)
+		if form.is_valid():
+			lineacompra = form.save(commit=False)
+			lineacompra.save()
+			return HttpResponseRedirect(self.get_success_url())
+		else:
+			return render_to_response(self.get_context_data(form=form))
+
+class ActualizarLineaCompra(UpdateView):
+	"""docstring for ActualizarLineaCompra"""
 	model = LineaDeCompra
 	template_name = 'CrearLineaDeCompra.html'
 	form_class = LineaDeCompraForm
@@ -78,3 +124,25 @@ class RegistrarIngrediente(CreateView):
 		else:
 			return render_to_response(self.get_context_data(form=form))
 
+class ActualizarIngrediente(UpdateView):
+	"""docstring for ActualizarIngrediente"""
+	model = Ingrediente
+	template_name = 'CrearIngrediente.html'
+	form_class = IngredienteForm
+	success_url = 'linea'
+
+	def get_context_data(self, **kwargs):
+		context = super(RegistrarIngrediente,self).get_context_data(**kwargs)
+		if 'form' not in context:
+			context['form'] = self.form_class(self.request.GET)
+		return context
+
+	def post(self, request, *args, **kwargs):
+		self.object = self.get_object
+		form = self.form_class(self.request.POST)
+		if form.is_valid():
+			ingrediente = form.save(commit=False)
+			ingrediente.save()
+			return HttpResponseRedirect(self.get_success_url())
+		else:
+			return render_to_response(self.get_context_data(form=form))
